de53b46799a705e58331f37e1f9603e67765f8ed
Peter-Yu-0402 <peter.yu.0402@gmail.com>
2024-08-11 11:38:10 +0900

    테스트코드 샘플 적용

---
+++ src/test/java/org/jaesung/springbootdeveloper/TestControllerTest.java
@@ -0,0 +1,64 @@
+package org.jaesung.springbootdeveloper;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest // 테스트용 애플리케이션 컨텍스트 생성
+@AutoConfigureMockMvc // MockMvc 생성
+class TestControllerTest {
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @BeforeEach // 테스트 실행 전 실행하는 메서드
+    public void mockMvcSetUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .build();
+    }
+
+    @AfterEach // 테스트 실행 후 실행하는 메서드
+    public void cleanUp() {
+        memberRepository.deleteAll();
+    }
+
+    @DisplayName("getAllMembers : 아티클 조회에 성공한다.")
+    @Test
+    public void getAllMembers() throws Exception {
+        // given
+        final String url = "/test";
+        Member savedMember = memberRepository.save(new Member(1L, "홍길동"));
+
+        // when
+        final ResultActions result = mockMvc.perform(get(url)
+                .accept(MediaType.APPLICATION_JSON));
+
+        // perform 메서드는 요청을 전송한다.
+        // 결과로 ResultActions 객체를 받으며, 해당 객체는 반환값을 검증하고 확인하는 메서드가 있다.
+
+        // then
+        result
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").value(savedMember.getId()))
+                .andExpect(jsonPath("$[0].name").value(savedMember.getName()));
+    }
+}
