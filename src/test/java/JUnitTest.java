96098494b2696d9850039c18a6d232479a12a84b
Peter-Yu-0402 <peter.yu.0402@gmail.com>
2024-08-11 11:37:40 +0900

    AssertJ

--- src/test/java/JUnitTest.java
+++ src/test/java/JUnitTest.java
@@ -2,6 +2,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class JUnitTest {
     @DisplayName("1 + 2는 3이다.") // 테스트 이름
     @Test // 해당 애너테이션을 붙인 메서드는 테스트를 수행하는 메서드가 된다.
@@ -12,15 +14,8 @@ public class JUnitTest {
 
         // 검증 메서드 사용
         Assertions.assertEquals(a + b, sum); // 값이 같은지 확인
-    }
 
-    @DisplayName("1 + 3는 4이다.")
-    @Test
-    public void junitFailedTest() {
-        int a = 1;
-        int b = 3;
-        int sum = 100;
-
-        Assertions.assertEquals(a + b, sum); // 실패하는 케이스
+        // 기댓값과 비교값이 가독성 있게 구분된 AssertJ
+        assertThat(a + b).isEqualTo(sum);
     }
 }
