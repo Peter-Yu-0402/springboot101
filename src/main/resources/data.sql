6024878dc8f7bcb5998cccde116d98a7af4e0dd4
Peter-Yu-0402 <peter.yu.0402@gmail.com>
2024-08-21 16:31:19 +0900

    SQL 주석 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,5 @@
-# h2 콘솔에 접속해 쿼리를 입력하여 데이터가 제대로 저장되었는지 확인해보자.
+/* h2 콘솔에 접속해 쿼리를 입력하여
+데이터가 제대로 저장되었는지 확인해보자. */
 INSERT INTO article (title, content) VALUES ('title 1', 'content 1')
 INSERT INTO article (title, content) VALUES ('title 2', 'content 2')
 INSERT INTO article (title, content) VALUES ('title 3', 'content 3')
